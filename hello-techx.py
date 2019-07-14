b73e05ead82463019bd08d3a5235c260078c8393
tiff-wang <wangtiffany@gmail.com>
2019-07-14 15:57:31 +0700

    added all needed import

--- hello-techx.py
+++ hello-techx.py
@@ -1,10 +1,23 @@
+# Python standard libraries
+import math
+import operator
+import os
+import pickle
+import warnings
+
+# Data Visualization libraries 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Data manipulation libraries
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import torch.nn as nn
 
-from keras.models import Sequential
-from sklearn.linear_model import LinearRegression
+# Machine learning libraries
+import cv2
+import keras 
+import sklearn 
+import torch.nn as nn
 
 print("Hello TechX Class of 2019...")
 print("If you have import errors please raise your hand and let one of us know!")
